6af358c9db80fe20fd2b55ce7c2ef6d4c2f5247e
siy0h <serenaiyoha@yahoo.ca>
2024-02-02 12:35:00 -0500

    side panel added (.js dont work tho) pages renamed so client ->

--- Lab3/app.js
+++ Lab3/app.js
@@ -204,6 +204,15 @@ function getTotalPrice(chosenProducts) {
     return totalPrice;
   }
 
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
 
 createProductList(products);
 createFoodCategories();
